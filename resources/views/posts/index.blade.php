1fcfaf892225f7cc8d9afa122194c32542a5fd95
niimurank <nk2002826@gmail.com>
2023-07-26 02:08:17 +0900

    css-update#6 ユーザー情報と投稿内容の分別作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,18 +8,22 @@
         <meta name="工事中">
     </head>
     <body>
-        <div class="container">
-            <h1 class="text-center">投稿</h1>
+        <div class="container mx-auto">
             <div class="posts">
                 @foreach($posts as $post)
-                <div class="post">
-                    <h2 class="user">user_id:{{ $post->user_id }}</h2>
-                    <p class="user_name">name:{{ $post->user->name }}</p>
-                    <p class="body">body:{{ $post->body }} </p>
-                    <p class="time">time:{{ $post->record->time }}</p>
-                    <p class="distance">distance:{{ $post->record->distance->swim_distance }}</p>
-                    <p class="style">style:{{ $post->record->style->style_name }}</p>
-                </div>
+                <article class="post flex flex-col p-2 text-center border bg-white">
+                    <div class="">
+                        <div class="profile flex">
+                        <img class="w-12 h-12 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar">
+                        <div class="user_name pt-2 font-semibold">{{ $post->user->name }}</div>
+                        </div>
+                        <h2 class="user">user_id:{{ $post->user_id }}</h2>
+                        <p class="body">body:{{ $post->body }} </p>
+                        <p class="time">time:{{ $post->record->time }}</p>
+                        <p class="distance">distance:{{ $post->record->distance->swim_distance }}</p>
+                        <p class="style">style:{{ $post->record->style->style_name }}</p>
+                    </div>
+                </article>
                 @endforeach
             </div>
         </div>
