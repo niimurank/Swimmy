4019cd6009aed55d2c652953efe4931e62a31327
niimurank <nk2002826@gmail.com>
2023-08-07 14:00:13 +0900

    fix-PostControllerのリクエスト数削減のためgetからwithに変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     public function index(Post $post){
         $posts = $post  ->with(['record.style', 'record.distance','user'])
                         ->orderBy('created_at', 'desc')
-                        ->get();
+                        ->with();
         return view('posts.index', ['posts' => $posts]);
     }
     
