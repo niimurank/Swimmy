e64da82b949ae1506e83bc048729ce009050b23e
niimurank <nk2002826@gmail.com>
2023-10-24 04:21:13 +0900

    headerを共通化

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,13 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <a href="{{ route('posts.index') }}">
-            <div class="flex">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 rounded-full hover:bg-gray-200">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-                </svg>
-                <h1 class="mt-0.5">戻る</h1>
-            </div>
-        </a>
+        <x-header />
     </x-slot>
     <div class="container mx-auto">
         <div class="post">
