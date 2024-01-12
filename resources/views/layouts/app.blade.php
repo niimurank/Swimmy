bbbb2cda85c2f4326cd72619d8cb36e5cb271ee0
niimurank <nk2002826@gmail.com>
2024-01-12 21:11:27 +0900

    fix-スクロールでメッセージを閲覧できるように変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
             @include('layouts.navigation')
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow border-b">
+                <header class="bg-white shadow border-b h-20">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         <!-- Navigation Links -->
                         {{ $header }}
--- resources/views/rooms/show.blade.php
+++ resources/views/rooms/show.blade.php
@@ -2,15 +2,15 @@
     <x-slot name="header">
         <x-header />
     </x-slot>
-    <div class="container mx-auto h-screen bg-white">
-        <section class="rooms p-2 text-center bg-white">
+    <div class="container mx-auto flex flex-col" style="height: calc(100vh - 10rem);">
+        <section class="messages p-2 text-center bg-white overflow-y-auto h-screen">
             <ul class="space-y-2">
                 @foreach ($messages as $message)
-                    <x-message :message="$message" />
+                    <x-message :message="$message"/>
                 @endforeach
             </ul>
         </section>
-        <form action="{{ route('messages.store') }}" method="POST" class="flex w-1/3 p-4 bottom-0 end-0 absolute bottom-0">
+        <form action="{{ route('messages.store') }}" method="POST" class="flex p-4 bottom-0 sticky bg-white">
                 @csrf
                 <input type="hidden" name="message[room_id]" value="{{ $room_id }}">
                 <textarea id="message" name="message[body]" rows="1" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="メッセージを入力..."></textarea>
@@ -19,4 +19,10 @@
                 </button>
             </form>
     </div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var messageContainer = document.querySelector(".messages");
+            messageContainer.scrollTop = messageContainer.scrollHeight;
+        });
+    </script>
 </x-app-layout>
